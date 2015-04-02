c87f9a3ab329c63de675725c5d83681ed4ed358f
Alex Tsankov <antsankov@gmail.com>
2015-04-02 22:33:58 +0000

    Added in map updater

--- static/js/rtdGps.js
+++ static/js/rtdGps.js
@@ -1,35 +1,35 @@
+var globalMap; 
+var markers = [];
+
+//our destinations, a dictionary of the stops
+var destinations = {
+    'boulder_transit_center' : new google.maps.LatLng(40.017103,-105.276444),
+    'broadway_16th' : new google.maps.LatLng(40.005663,-105.272381),
+    'broadway_27th' : new google.maps.LatLng(39.996074,-105.261018)
+}
+
+var destination = destinations['broadway_27th'];
+
+//these are the options for getting the gps coordinates 
 var options = {
   enableHighAccuracy: true,
-  timeout: 5000,
+  timeout: 30000,
   maximumAge: 0
 };
 
 // a simple wrapper function around computeDistance that takes a tolerance. EVERYTHING IN METERS
 function calcDistance(loc1,loc2,tolerance) {
   var distance = google.maps.geometry.spherical.computeDistanceBetween(loc1, loc2);
-  
   if (distance <= tolerance)
-    return "TIME TO WAKE UP"
+    console.log("TIME TO WAKE UP");
   else
-    return "Everything ok"
+    console.log("Everything ok");
 }
 
-//this is where we make the actual map
-function generateMap(){
-  //calculate our current position and run some other stuff. We might need to loop this every 10 seconds.
+function initializeMap(){ 
   function success(pos) {
     var crd = pos.coords;
     var current = new google.maps.LatLng(crd.latitude,crd.longitude);
-
-    //our destinations, a dictionary of the stops
-    var destinations = {
-                       'boulder_transit_center' : new google.maps.LatLng(40.017103,-105.276444),
-                       'broadway_16th' : new google.maps.LatLng(40.005663,-105.272381),
-                       'broadway_27th' : new google.maps.LatLng(39.996074,-105.261018)
-                       }
-
-    //we select the stop we want to go to.
-    var destination = destinations['broadway_27th'];
 
     //the map options for google maps
     var mapOptions = {
@@ -37,38 +37,65 @@ function generateMap(){
       zoom: 14
     }
 
-    console.log(calcDistance(current,destination,50))
-
-    console.log(mapOptions)
-    console.log('Your current position is:');
-    console.log('Latitude : ' + crd.latitude);
-    console.log('Longitude: ' + crd.longitude);
-    console.log('More or less ' + crd.accuracy + ' meters.');
-
     //instantiate the google map object
-    var map = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+    globalMap = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
 
     //some markers on the map
-    var destination_marker = new google.maps.Marker({
+    var destinationMarker = new google.maps.Marker({
       position: destination,
-      map: map,
+      map: globalMap,
       title: "Where I am going!"
     });
+    markers.push(destinationMarker)
 
     var current_marker = new google.maps.Marker({
       position: current,
-      map: map,
+      map: globalMap,
       title:"Hello World!"
     });
+    markers.push(current_marker)
+    
   };
 
   function error(err) {
     console.warn('ERROR(' + err.code + '): ' + err.message);
   };
+  navigator.geolocation.getCurrentPosition(success, error, options);
+}
 
+//this is where we make the actual map
+function updateMap(){
+  //calculate our current position and run some other stuff. We might need to loop this every 10 seconds.
+  function success(pos) {
+    
+    var crd = pos.coords;
+
+    var current = new google.maps.LatLng(crd.latitude,crd.longitude);
+    globalMap.panTo(current)
+
+    calcDistance(current,destination,500)
+    console.log(crd.latitude);
+   
+    markers.pop().setMap(null);
+    
+    var current_marker = new google.maps.Marker({
+      position: current,
+      map: globalMap,
+      title:current.toString()
+    }); 
+    markers.push(current_marker);
+  };
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
   //this actually gets our location
   navigator.geolocation.getCurrentPosition(success, error, options);
 }
 
-//this embedds the object in the page
-google.maps.event.addDomListener(window, 'load', generateMap());
+google.maps.event.addDomListener(window, 'load', initializeMap());
+
+window.setInterval(function(){
+    //this updates the map every 5 seconds
+    updateMap()   
+},5000);

178df8302c0ffd5de25ab28d260c84846e462102
Alexander Tsankov <antsankov@gmail.com>
2015-04-07 13:42:38 -0600

    Added in nice bootstrap template

--- server.py
+++ server.py
@@ -1,9 +1,14 @@
 from flask import *
 app = Flask(__name__)
 
+@app.route('/nice')
+def return_nice(name=None): 
+    return render_template('index.html', name=name)
+
 @app.route('/')
-def hello_world(name=None): 
+def return_working(name=None): 
     return render_template('test.html', name=name)
+
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',)

71d284ed3355a8f25447274d87527200016165f5
Alexander Tsankov <antsankov@gmail.com>
2015-04-07 13:52:40 -0600

    Added in nice map. More work to be done

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,27 +1,108 @@
+var globalMap; 
+var markers = [];
 
-$(document).ready(function(){/* google maps -----------------------------------------------------*/
-google.maps.event.addDomListener(window, 'load', initialize);
+//our destinations, a dictionary of the stops
+var destinations = {
+    'boulder_transit_center' : new google.maps.LatLng(40.017103,-105.276444),
+    'broadway_16th' : new google.maps.LatLng(40.005663,-105.272381),
+    'broadway_27th' : new google.maps.LatLng(39.996074,-105.261018)
+}
 
-function initialize() {
+var destination = destinations['broadway_27th'];
 
-  /* position Amsterdam */
-  var latlng = new google.maps.LatLng(52.3731, 4.8922);
+//these are the options for getting the gps coordinates 
+var options = {
+  enableHighAccuracy: true,
+  timeout: 30000,
+  maximumAge: 0
+};
 
-  var mapOptions = {
-    center: latlng,
-    scrollWheel: false,
-    zoom: 13
+// a simple wrapper function around computeDistance that takes a tolerance. EVERYTHING IN METERS
+function calcDistance(loc1,loc2,tolerance) {
+  var distance = google.maps.geometry.spherical.computeDistanceBetween(loc1, loc2);
+  if (distance <= tolerance)
+    console.log("TIME TO WAKE UP");
+  else
+    console.log("Everything ok");
+}
+
+
+//this is where we make the actual map
+function updateMap(){
+  //calculate our current position and run some other stuff. We might need to loop this every 10 seconds.
+  function success(pos) {
+    
+    var crd = pos.coords;
+
+    var current = new google.maps.LatLng(crd.latitude,crd.longitude);
+    globalMap.panTo(current)
+
+    calcDistance(current,destination,500)
+    console.log(crd.latitude);
+   
+    markers.pop().setMap(null);
+    
+    var current_marker = new google.maps.Marker({
+      position: current,
+      map: globalMap,
+      title:current.toString()
+    }); 
+    markers.push(current_marker);
   };
-  
-  var marker = new google.maps.Marker({
-    position: latlng,
-    url: '/',
-    animation: google.maps.Animation.DROP
-  });
-  
-  var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-  marker.setMap(map);
+
+  function error(err) {
+    console.warn('ERROR(' + err.code + '): ' + err.message);
+  };
+  //this actually gets our location
+  navigator.geolocation.getCurrentPosition(success, error, options);
 
 };
+
+
+$(document).ready(function(){/* google maps -----------------------------------------------------*/
+  google.maps.event.addDomListener(window, 'load', initialize);
+
+  function initialize() {
+
+      function success(pos) {
+      var crd = pos.coords;
+      var current = new google.maps.LatLng(crd.latitude,crd.longitude);
+
+      //the map options for google maps
+      var mapOptions = {
+        center: current,
+        zoom: 14
+      }
+
+      //instantiate the google map object
+      globalMap = new google.maps.Map(document.getElementById('map-canvas'),mapOptions);
+
+      //some markers on the map
+      var destinationMarker = new google.maps.Marker({
+        position: destination,
+        map: globalMap,
+        title: "Where I am going!"
+      });
+      markers.push(destinationMarker)
+
+      var current_marker = new google.maps.Marker({
+        position: current,
+        map: globalMap,
+        title:"Hello World!"
+      });
+      markers.push(current_marker)
+      
+    };
+
+    function error(err) {
+      console.warn('ERROR(' + err.code + '): ' + err.message);
+    };
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }
 /* end google maps -----------------------------------------------------*/
 });
+
+window.setInterval(function(){
+    //this updates the map every 5 seconds
+    updateMap()   
+},5000);
